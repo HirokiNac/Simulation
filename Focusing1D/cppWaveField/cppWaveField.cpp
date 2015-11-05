4fe42b0b7f8b8e66085cb9c18935cd3e8cd5ec75
Hiroki <hirokinn@rg7.so-net.ne.jp>
2015-11-05 09:55:27 +0900

    cpp追加

---
+++ Focusing1D/cppWaveField/cppWaveField.cpp
@@ -0,0 +1,13 @@
+// これは メイン DLL ファイルです。
+
+#include "stdafx.h"
+#include <complex>
+#include "cppWaveField.h"
+
+void cppWaveField::WF::fProp(array<double>^ x1, array<double>^ y1, array<double>^ real1, array<double>^ imag1, array<double>^ x2, array<double>^ y2, array<double>^ real2, array<double>^ imag2)
+{
+	std::complex<double> *u1;
+
+
+	std::complex<double> *u2;
+}
---
+++ Focusing1D/cppWaveField/cppWaveField.h
@@ -0,0 +1,16 @@
+// cppWaveField.h
+
+#pragma once
+
+using namespace System;
+
+namespace cppWaveField {
+
+	public ref class WF
+	{
+		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
+	public:
+		void fProp(array<double>^x1, array<double>^y1, array<double>^real1, array<double>^imag1,
+			array<double>^x2, array<double>^y2, array<double>^real2, array<double>^imag2);
+	};
+}
