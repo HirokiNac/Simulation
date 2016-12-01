c5602d891a2c90763e9ba9cd29c276dbde386e7a
hirokinn <hirokinn@rg7.so-net.ne.jp>
2016-12-01 18:29:58 +0900

    20161201

---
+++ ClsNac.WO/ClsNac.WO/ClsNac.WO.cpp
@@ -0,0 +1,19 @@
+// これは メイン DLL ファイルです。
+
+#include "stdafx.h"
+
+#include "ClsNac.WO.h"
+
+template <typename T>
+ClsNac::Mirror2D::Mirror2D(T _L1, T _L2, T theta,fig _fig)
+{
+	switch (_fig)
+	{
+	case ellip:
+		break;
+	case plane:
+		break;
+	default:
+		break;
+	}
+}
