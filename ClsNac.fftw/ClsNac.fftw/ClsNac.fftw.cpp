e45160b890361f750b1300324bea51a184567a45
hiroki <hiroki@HIROKI-PC>
2017-02-23 12:24:24 +0900

    2017/02/23

--- ClsNac.fftw/ClsNac.fftw/ClsNac.fftw.cpp
+++ ClsNac.fftw/ClsNac.fftw/ClsNac.fftw.cpp
@@ -1,10 +1,12 @@
 // これは メイン DLL ファイルです。
 
 #include "stdafx.h"
-#include<fftw3.h>
 #include "ClsNac.fftw.h"
+#pragma comment(lib, "libfftw3-3.lib")
+#pragma comment(lib, "libfftw3f-3.lib")
+#pragma comment(lib, "libfftw3l-3.lib")
 
-void ClsNac::fftw::Execute1D(array<Complex>^ _in, array<Complex>^ _out, int _sign)
+void ClsNac::fftw::Execute1D(array<Complex>^ _in, array<Complex>^ _out, int _dir)
 {
 	int len = _in->Length;
 
@@ -20,13 +22,23 @@ void ClsNac::fftw::Execute1D(array<Complex>^ _in, array<Complex>^ _out, int _sig
 	fftw_complex *out_fftw = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*len);
 
 	//FFTプラン
-	fftw_plan p = fftw_plan_dft_1d(len, in_fftw, out_fftw, _sign, FFTW_ESTIMATE);
+	fftw_plan p = fftw_plan_dft_1d(len, in_fftw, out_fftw,_dir, FFTW_ESTIMATE);
 	fftw_execute(p);
-	
-	double len_2 = 1.0 / Math::Sqrt(len);
-	for (int i = 0; i < len; i++)
+
+	if (_dir == FFTW_BACKWARD)
 	{
-		_out[i] = Complex(out_fftw[i][0] * len_2, out_fftw[i][1] * len_2);
+		double len_2 = 1.0 / Math::Sqrt(len);
+		for (int i = 0; i < len; i++)
+		{
+			_out[i] = Complex(out_fftw[i][0] / len_2, out_fftw[i][1]);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < len; i++)
+		{
+			_out[i] = Complex(out_fftw[i][0], out_fftw[i][1]);
+		}
 	}
 
 	fftw_destroy_plan(p);
@@ -34,19 +46,225 @@ void ClsNac::fftw::Execute1D(array<Complex>^ _in, array<Complex>^ _out, int _sig
 	fftw_free(out_fftw);
 
 }
-void ClsNac::fftw::Execute1D(array<Complex>^ _in, array<double>^ _out, int _sign)
+
+void ClsNac::fftw::Forward1D(array<double>^_in, array<Complex>^_out)
 {
+	int len = _in->Length;
+
+	//FFT入力用
+	double *in_fftw = (double*)fftw_malloc(sizeof(double)*len);
+	for (int i = 0; i < len; i++)
+	{
+		in_fftw[i] = _in[i];
+	}
+
+	//FFT結果
+	fftw_complex *out_fftw = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*len);
+
+	//FFTプラン
+	fftw_plan p = fftw_plan_dft_r2c_1d(len, in_fftw, out_fftw, FFTW_ESTIMATE);
+	fftw_execute(p);
+
+	for (int i = 0; i < len; i++)
+		_out[i] = Complex(out_fftw[i][0], out_fftw[i][1]);
+
+	fftw_destroy_plan(p);
+	fftw_free(in_fftw);
+	fftw_free(out_fftw);
+
 }
 
-void ClsNac::fftw::Execute2D(array<Complex>^ _in, array<Complex>^ _out, int _sign, bool _shift)
+void ClsNac::fftw::Backward1D(array<Complex>^ _in, array<double>^ _out)
 {
+	int len = _in->Length;
+
+	//FFT入力用
+	fftw_complex *in_fftw = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*len);
+	for (int i = 0; i < len; i++)
+	{
+		in_fftw[i][0] = _in[i].Real;
+		in_fftw[i][1] = _in[i].Imaginary;
+	}
+
+	//FFT結果
+	double *out_fftw = (double*)fftw_malloc(sizeof(double)*len);
+
+	//FFTプラン
+	fftw_plan p = fftw_plan_dft_c2r_1d(len, in_fftw, out_fftw, FFTW_ESTIMATE);
+	fftw_execute(p);
+
+	double len_2 = 1.0 / Math::Sqrt(len);
+	for (int i = 0; i < len; i++)
+		_out[i] = out_fftw[i] / len_2;
+
+	fftw_destroy_plan(p);
+	fftw_free(in_fftw);
+	fftw_free(out_fftw);
+
 }
 
-void ClsNac::fftw::Execute2D(array<double>^ _in, array<Complex>^ _out, int _sign, bool _shift)
+
+void ClsNac::fftw::Execute2D(array<Complex, 2>^ _in, array<Complex, 2>^ _out, int _dir, bool _shift)
 {
+	int len1 = _in->GetLength(0);
+	int len2 = _in->GetLength(1);
+
+	//FFT入力用
+	fftw_complex *in_fftw = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*len1*len2);
+	for (int i = 0; i < len1; i++)
+		for (int j = 0; j < len2; j++)
+		{
+			in_fftw[i*len2 + j][0] = _in[i, j].Real;
+			in_fftw[i*len2 + j][1] = _in[i, j].Imaginary;
+		}
+
+	fftw_complex *out_fftw = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*len1*len2);
+
+	fftw_plan p = fftw_plan_dft_2d(len1, len2, in_fftw, out_fftw, _dir, FFTW_ESTIMATE);
+	fftw_execute(p);
+
+
+	if (_dir == FFTW_BACKWARD)
+	{
+		//BACKWARD
+		double len_2 = 1.0 / Math::Sqrt(len1*len2);
+
+		for (int i = 0; i < len1 / 2; i++)
+			for (int j = 0; j < len2 / 2; j++)
+			{
+				if (_shift)
+				{
+					//shiftあり
+					_out[i, j] = Complex(out_fftw[i + len1 / 2, j + len2 / 2][0] * len_2, out_fftw[i + len1 / 2, j + len2 / 2][1]);
+					_out[i + len1 / 2, j] = Complex(out_fftw[i, j + len2 / 2][0] * len_2, out_fftw[i, j + len2 / 2][1]);
+					_out[i, j + len2 / 2] = Complex(out_fftw[i + len1 / 2, j][0] * len_2, out_fftw[i + len1 / 2, j][1] );
+					_out[i + len1 / 2, j + len2 / 2] = Complex(out_fftw[i, j][0] * len_2, out_fftw[i, j][1] );
+				}
+				else
+				{
+					//shiftなし
+					_out[i, j] = Complex(out_fftw[i*len2 + j][0] * len_2, out_fftw[i*len2 + j][1]);
+					_out[i + len1 / 2, j] = Complex(out_fftw[(i + len1 / 2)*len2 + j][0] * len_2, out_fftw[(i + len1 / 2)*len2 + j][1]);
+					_out[i, j + len2 / 2] = Complex(out_fftw[i*len2 + j + len2 / 2][0] * len_2, out_fftw[i*len2 + j + len2 / 2][1]);
+					_out[i + len1 / 2, j + len2 / 2] = Complex(out_fftw[(i + len1 / 2)*len2 + j + len2 / 2][0] * len_2, out_fftw[(i + len1 / 2)*len2 + j + len2 / 2][1]);
+				}
+			}
+	}
+	else
+	{
+		//FORWARD
+		for (int i = 0; i < len1 / 2; i++)
+			for (int j = 0; j < len2 / 2; j++)
+			{
+				if (_shift)
+				{
+					//shiftあり
+					_out[i, j] = Complex(out_fftw[i + len1 / 2, j + len2 / 2][0], out_fftw[i + len1 / 2, j + len2 / 2][1]);
+					_out[i + len1 / 2, j] = Complex(out_fftw[i, j + len2 / 2][0], out_fftw[i, j + len2 / 2][1]);
+					_out[i, j + len2 / 2] = Complex(out_fftw[i + len1 / 2, j][0], out_fftw[i + len1 / 2, j][1]);
+					_out[i + len1 / 2, j + len2 / 2] = Complex(out_fftw[i, j][0], out_fftw[i, j][1]);
+				}
+				else
+				{
+					//shiftなし
+					_out[i, j] = Complex(out_fftw[i*len2 + j][0], out_fftw[i*len2 + j][1]);
+					_out[i + len1 / 2, j] = Complex(out_fftw[(i + len1 / 2)*len2 + j][0], out_fftw[(i + len1 / 2)*len2 + j][1]);
+					_out[i, j + len2 / 2] = Complex(out_fftw[i*len2 + j + len2 / 2][0], out_fftw[i*len2 + j + len2 / 2][1]);
+					_out[i + len1 / 2, j + len2 / 2] = Complex(out_fftw[(i + len1 / 2)*len2 + j + len2 / 2][0], out_fftw[(i + len1 / 2)*len2 + j + len2 / 2][1]);
+				}
+			}
+	}
+
 }
 
-void ClsNac::fftw::Execute2D(array<Complex>^ _in, array<double>^ _out, int _sign, bool _shift)
+
+void ClsNac::fftw::Forward2D(array<double, 2>^ _in, array<Complex, 2>^ _out, bool _shift)
 {
+	int len1 = _in->GetLength(0);
+	int len2 = _in->GetLength(1);
+
+	//FFT入力用
+	double *in_fftw = (double*)fftw_malloc(sizeof(double)*len1*len2);
+	for (int i = 0; i < len1; i++)
+		for (int j = 0; j < len2; j++)
+		{
+			in_fftw[i*len2 + j] = _in[i, j];
+		}
+
+	fftw_complex *out_fftw = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*len1*len2);
+
+	fftw_plan p = fftw_plan_dft_r2c_2d(len1, len2, in_fftw, out_fftw, FFTW_ESTIMATE);
+	fftw_execute(p);
+
+
+	//FORWARD
+	for (int i = 0; i < len1 / 2; i++)
+		for (int j = 0; j < len2 / 2; j++)
+		{
+			double re, im;
+			
+			
+			if (_shift)
+			{
+				//shiftあり
+				_out[i, j] = Complex(out_fftw[i + len1 / 2, j + len2 / 2][0], out_fftw[i + len1 / 2, j + len2 / 2][1]);
+				_out[i + len1 / 2, j] = Complex(out_fftw[i, j + len2 / 2][0], out_fftw[i, j + len2 / 2][1]);
+				_out[i, j + len2 / 2] = Complex(out_fftw[i + len1 / 2, j][0], out_fftw[i + len1 / 2, j][1]);
+				_out[i + len1 / 2, j + len2 / 2] = Complex(out_fftw[i, j][0], out_fftw[i, j][1]);
+			}
+			else
+			{
+				//shiftなし
+				_out[i, j] = Complex(out_fftw[i*len2 + j][0], out_fftw[i*len2 + j][1]);
+				_out[i + len1 / 2, j] = Complex(out_fftw[i + len1 / 2, j][0], out_fftw[i + len1 / 2, j][1]);
+				_out[i, j + len2 / 2] = Complex(out_fftw[i, j + len2 / 2][0], out_fftw[i, j + len2 / 2][1]);
+				_out[i + len1 / 2, j + len2 / 2] = Complex(out_fftw[i + len1 / 2, j + len2 / 2][0], out_fftw[i + len1 / 2, j + len2 / 2][1]);
+			}
+		}
+}
+
+void ClsNac::fftw::Backward2D(array<Complex,2>^ _in, array<double,2>^ _out, bool _shift)
+{
+	int len1 = _in->GetLength(0);
+	int len2 = _in->GetLength(1);
+
+	//FFT入力用
+	fftw_complex *in_fftw = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*len1*len2);
+	for (int i = 0; i < len1; i++)
+		for (int j = 0; j < len2; j++)
+		{
+			in_fftw[i*len2 + j][0] = _in[i, j].Real;
+			in_fftw[i*len2 + j][1] = _in[i, j].Imaginary;
+		}
+
+	double *out_fftw = (double*)fftw_malloc(sizeof(double)*len1*len2);
+
+	fftw_plan p = fftw_plan_dft_c2r_2d(len1, len2, in_fftw, out_fftw, FFTW_ESTIMATE);
+	fftw_execute(p);
+
+
+		//BACKWARD
+		double len_2 = 1.0 / Math::Sqrt(len1*len2);
+
+		for (int i = 0; i < len1 / 2; i++)
+			for (int j = 0; j < len2 / 2; j++)
+			{
+				if (_shift)
+				{
+					//shiftあり
+					_out[i, j] = out_fftw[i + len1 / 2, j + len2 / 2] * len_2;
+					_out[i + len1 / 2, j] = out_fftw[i, j + len2 / 2] * len_2;
+					_out[i, j + len2 / 2] = out_fftw[i + len1 / 2, j] * len_2;
+					_out[i + len1 / 2, j + len2 / 2] = out_fftw[i, j] * len_2;
+				}
+				else
+				{
+					//shiftなし
+					_out[i, j] = out_fftw[i*len2 + j] * len_2;
+					_out[i + len1 / 2, j] = out_fftw[i + len1 / 2, j] * len_2;
+					_out[i, j + len2 / 2] = out_fftw[i, j + len2 / 2] * len_2;
+					_out[i + len1 / 2, j + len2 / 2] = out_fftw[i + len1 / 2, j + len2 / 2] * len_2;
+				}
+			}
 }
 
--- ClsNac.fftw/ClsNac.fftw/ClsNac.fftw.h
+++ ClsNac.fftw/ClsNac.fftw/ClsNac.fftw.h
@@ -1,6 +1,8 @@
 // ClsNac.fftw.h
 
 #pragma once
+#include<complex.h>
+#include<fftw3.h>
 
 using namespace System;
 using System::Numerics::Complex;
@@ -12,13 +14,32 @@ namespace ClsNac {
 		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
 
 	public:
-		static void Execute1D(array<Complex>^ _in, array<Complex>^_out, int _sign);
-		static void Execute1D(array<double>^ _in, array<Complex>^_out, int _sign);
-		static void Execute1D(array<Complex>^ _in, array<double>^_out, int _sign);
+
+		/// <summary>
+		/// 1DFFT
+		/// </summary>
+		/// <param name="_in"></param>
+		/// <param name="_out"></param>
+		/// <param name="_dir">FFT:-1,iFFT:1</param>
+		static void Execute1D(array<Complex>^ _in, array<Complex>^_out, int _dir);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="_in"></param>
+		/// <param name="_out"></param>
+		static void Forward1D(array<double>^ _in, array<Complex>^_out);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="_in"></param>
+		/// <param name="_out"></param>
+		static void Backward1D(array<Complex>^ _in, array<double>^_out);
 		
-		static void Execute2D(array<Complex>^ _in, array<Complex>^_out, int _sign, bool _shift);
-		static void Execute2D(array<double>^ _in, array<Complex>^_out, int _sign, bool _shift);
-		static void Execute2D(array<Complex>^ _in, array<double>^_out, int _sign, bool _shift);
+		static void Execute2D(array<Complex,2>^ _in, array<Complex,2>^_out, int _dir, bool _shift);
+		static void Forward2D(array<double,2>^ _in, array<Complex,2>^_out,bool _shift);
+		static void Backward2D(array<Complex,2>^ _in, array<double,2>^_out, bool _shift);
 		
 		
 	};
