1fb14b5387d745e2f5e157950b378428d8b3db17
xeon <xeon@XEON-PC>
2017-03-14 20:53:01 +0900

    2017/03/14_3

--- ClsNac.WaveOptics/cppWaveOptics/cppWaveOptics.h
+++ ClsNac.WaveOptics/cppWaveOptics/cppWaveOptics.h
@@ -17,9 +17,14 @@ namespace ClsNac {
 		double k;
 
 	public:
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="_lambda"></param>
+		/// <param name="_Core">0:AllProcs</param>
 		cppWaveOptics(double _lambda,int _Core)
 		{
-			if (_Core == 0)
+			if (_Core == 0 || _Core > omp_get_num_procs())
 				Core = omp_get_num_procs();
 			else
 				Core = _Core;
@@ -39,8 +44,7 @@ namespace ClsNac {
 		/// <summary>
 		/// 
 		/// </summary>
-		/// <param name="_lambda"></param>
-		/// <param name="_dir"></param>
+		/// <param name="_dir">FW:-1 BW:1</param>
 		/// <param name="_x1"></param>
 		/// <param name="_y1"></param>
 		/// <param name="_u1"></param>
@@ -48,8 +52,12 @@ namespace ClsNac {
 		/// <param name="_y2"></param>
 		/// <param name="_u2"></param>
 		void Propagate1D(int _dir,
-			array<double>^ _x1, array<double>^ _y1, array<Complex>^ _u1,
-			array<double>^ _x2, array<double>^ _y2, array<Complex>^ _u2);
+			 array<double>^ _x1, array<double>^ _y1, array<Complex>^ _u1,
+			 array<double>^ _x2, array<double>^ _y2, array<Complex>^ _u2);
+
+		void Propagate1D(int _dir,
+			array<double>^ _x1, array<double>^ _y1, array<double>^ _u1r, array<double>^ _u1i,
+			array<double>^ _x2, array<double>^ _y2, array<double>^ _u2r, array<double>^_u2i);
 
 		/// <summary>
 		/// 
@@ -63,9 +71,9 @@ namespace ClsNac {
 		/// <param name="_y2"></param>
 		/// <param name="_z2"></param>
 		/// <param name="_u2"></param>
-		void Propagate2D(int _dir,
-			array<double>^_x1, array<double>^_y1, array<double>^_z1, array<Complex>^_u1,
-			array<double>^_x2, array<double>^_y2, array<double>^_z2, array<Complex>^_u2);
+		void Propagate2D( int _dir,
+			 array<double>^_x1,  array<double>^_y1,  array<double>^_z1,  array<Complex>^_u1,
+			 array<double>^_x2,  array<double>^_y2,  array<double>^_z2, array<Complex>^_u2);
 
 	};
 }
